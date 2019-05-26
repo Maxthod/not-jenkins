82a267bed17127e18f1f3cd4ca04a3de8e64dbdf
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-05-26 12:59:09 -0400

    Hello

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ function getImageNameGithub( payload ) {
 
 app.get('/info', async function (req, res) {
 
-    res.send("Sans chnanimagan 222289 ");
+    res.send("Je suis not jenkins ");
 
 });
 
