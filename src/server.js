ac27ad40ed0d904c5ff4ca3e210e8c5454e274a5
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-05-25 21:16:04 -0400

    Should work

--- src/server.js
+++ src/server.js
@@ -192,7 +192,7 @@ app.post('/not-jenkins-dev', async function (req, res) {
             const commandPush = `
                 set -e
                 if [ ! -f ~/.docker/config.json ] || ! grep -q "index.docker.io" ~/.docker/config.json; then 
-                   docker login --username "$DOCKER_USER" --password "$DOCKER_PASSWORD"
+                   docker login --username "$DOCKER_USER" --password-stdin <<< "$DOCKER_PASSWORD" > /dev/null
                 fi
                 docker push ${imageName}
             `
