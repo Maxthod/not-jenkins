6c72e445e646129392692cd2b7d9279b29b21d59
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-06-01 15:56:51 -0400

    Pipeline

--- src/main/controllers/TheHempathyController.js
+++ src/main/controllers/TheHempathyController.js
@@ -42,6 +42,45 @@ router.route('/backend-express')
         return;
     });
 
+
+    router.route('/frontend-react')
+    .post(async function (req, res) {
+        Logger.silly(`
+    ##################################################################################################################################
+    ##################################################################################################################################
+    ############################################# DEPLOYMENT THE HEMPATHY FRONTEND REACT ############################################
+    ##################################################################################################################################
+    ##################################################################################################################################
+    `);
+        try {
+            const options = {
+                scm: SCM.GITHUB,
+                source_branch: "develop",
+                workdir: process.env.THE_HEMPATHY_FRONTEND_REACT_WORKDIR || "thehempathy_frontend_react",
+                image_name: process.env.THE_HEMPATHY_FRONTEND_REACT_IMAGE_NAME || "thehempathy-frontend-react:latest",
+                service_name: process.env.THE_HEMPATHY_FRONTEND_REACT_SERVICE_NAME || "thehempathy_frontend_react",
+                secret: process.env.THE_HEMPATHY_FRONTEND_REACT_GITHUB_SECRET || "changeme"
+            }
+
+            GitHubUtil.startDeploy(req, options);
+
+            res.json({
+                status: 200,
+                message: "Deploy with success!"
+            })
+
+        } catch (err) {
+            Logger.error("Failed to deploy /not-jenkins. Error : %o", err);
+            next({
+                status: 400,
+            })
+        }
+
+
+        return;
+    });
+   
+
 module.exports = router;
 
 
